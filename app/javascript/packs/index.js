598ccd1b63b9b5abce9ecf581b6bf1d9e32c847d
notjacoob <ds0293097@gmail.com>
2022-08-09 20:05:06 -0700

    add cookies disclaimer

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,4 +1,19 @@
-document.addEventListener("turbolinks:load", () => {
+$(document).on('turbolinks:load', () => {
+
+    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|OperaMini/i.test(navigator.userAgent)) {
+        $("#cookies-popup-box").css("opacity", "0")
+        $("#cookies-disclaimer").on("mouseover", () => {
+            $("#cookies-popup-box").css("opacity", "100")
+        })
+        $("#cookies-disclaimer").on("mouseout", () => {
+            $("#cookies-popup-box").css("opacity", "0")
+        })
+    }
+
+    $("#cookies-disclaimer").on("click", () => {
+        document.cookie = "cookiesAllowed=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+    })
+
 
 })
 
